595a761b8e80d7372d7adfb505ce21e0a9aa8da1
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-08-10 08:49:21 -0400

    add basic game contract

--- contracts/script/Pictionary.s.sol
+++ contracts/script/Pictionary.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Pictionary} from "../src/Pictionary.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract PictionaryScript is Script {
+    Pictionary public game;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        game = new Pictionary();
 
         vm.stopBroadcast();
     }
